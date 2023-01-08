ce41dc4d5bfd6ebc46225246b6dc404e0d9d10bc
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2023-01-08 16:45:44 +0530

    Add files via upload

---
+++ 15_display_letters_to_string.py
@@ -0,0 +1,13 @@
+# name="python"
+# for n in name:
+#     print(n)
+
+""" In Reverse order """
+# name="python"
+# for n in name[::-1]:
+#     print(n)
+
+""" Printing the words alternatively"""
+# name="python"
+# for n in name[ : : 2 ]:
+#     print(n)
